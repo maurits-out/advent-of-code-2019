9e69c602863334df68870a3e6fe502316b7a58a6
maurits <17969131+maurits-out@users.noreply.github.com>
2021-01-09 21:34:21 +0100

    Day 3.

--- src/test/java/day03/CrossedWiresTest.java
+++ src/test/java/day03/CrossedWiresTest.java
@@ -12,7 +12,9 @@ import static support.IO.readInputAsLines;
 @DisplayName("Day 3: Crossed Wires")
 public class CrossedWiresTest {
 
-    private final List<String> example = List.of("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7");
+    private final List<String> example = List.of(
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7");
     private final List<String> input = readInputAsLines("day03-input.txt");
 
     @Nested
