be2ee77717f0c45b3496b7c0e592480ebed007fa
maurits <17969131+maurits-out@users.noreply.github.com>
2021-01-29 21:37:42 +0100

    Refactoring.

---
+++ src/main/java/day02/IntcodeComputer.java
@@ -0,0 +1,41 @@
+package day02;
+
+import java.util.Map;
+import java.util.function.BinaryOperator;
+import java.util.regex.Pattern;
+
+class IntcodeComputer {
+
+    private static final Map<Integer, BinaryOperator<Integer>> OPERATORS =
+            Map.of(1, Integer::sum, 2, (x, y) -> x * y);
+
+    private final String program;
+    private int[] memory;
+
+    IntcodeComputer(String program) {
+        this.program = program;
+    }
+
+    int run(int noun, int verb) {
+        createInitialState(noun, verb);
+        var ip = 0;
+        while (memory[ip] != 99) {
+            applyFunction(memory, ip, OPERATORS.get(memory[ip]));
+            ip += 4;
+        }
+        return memory[0];
+    }
+
+    private void applyFunction(int[] memory, int ip, BinaryOperator<Integer> function) {
+        var par1 = memory[ip + 1];
+        var par2 = memory[ip + 2];
+        var par3 = memory[ip + 3];
+        memory[par3] = function.apply(memory[par1], memory[par2]);
+    }
+
+    private void createInitialState(int noun, int verb) {
+        memory = Pattern.compile(",").splitAsStream(program).mapToInt(Integer::valueOf).toArray();
+        memory[1] = noun;
+        memory[2] = verb;
+    }
+}
--- src/main/java/day02/ProgramAlarm.java
+++ src/main/java/day02/ProgramAlarm.java
@@ -1,15 +1,9 @@
 package day02;
 
-import java.util.Map;
-import java.util.function.BinaryOperator;
-import java.util.regex.Pattern;
-
 import static java.util.stream.IntStream.rangeClosed;
 import static support.Pair.pair;
 
 public class ProgramAlarm {
-
-    private static final Map<Integer, BinaryOperator<Integer>> OPERATORS = Map.of(1, Integer::sum, 2, (x, y) -> x * y);
 
     private final String input;
 
@@ -22,33 +16,16 @@ public class ProgramAlarm {
     }
 
     public int part2() {
-        return rangeClosed(0, 99).boxed().flatMap(noun -> rangeClosed(0, 99).mapToObj(verb -> pair(noun, verb)))
+        return rangeClosed(0, 99)
+                .boxed().flatMap(noun -> rangeClosed(0, 99).mapToObj(verb -> pair(noun, verb)))
                 .filter(p -> runProgram(p.first, p.second) == 19690720)
-                .findAny().map(p -> p.first * 100 + p.second)
+                .findAny()
+                .map(p -> p.first * 100 + p.second)
                 .orElseThrow();
     }
 
     private int runProgram(int noun, int verb) {
-        var memory = createInitialState(noun, verb);
-        var ip = 0;
-        while (memory[ip] != 99) {
-            applyFunction(memory, ip, OPERATORS.get(memory[ip]));
-            ip += 4;
-        }
-        return memory[0];
-    }
-
-    private void applyFunction(int[] memory, int ip, BinaryOperator<Integer> function) {
-        var par1 = memory[ip + 1];
-        var par2 = memory[ip + 2];
-        var par3 = memory[ip + 3];
-        memory[par3] = function.apply(memory[par1], memory[par2]);
-    }
-
-    private int[] createInitialState(int noun, int verb) {
-        var program = Pattern.compile(",").splitAsStream(input).mapToInt(Integer::valueOf).toArray();
-        program[1] = noun;
-        program[2] = verb;
-        return program;
+        IntcodeComputer computer = new IntcodeComputer(input);
+        return computer.run(noun, verb);
     }
 }
