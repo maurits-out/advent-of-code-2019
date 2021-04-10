87bfb652562b941e4656fde46e27a4ae941f4f32
maurits <17969131+maurits-out@users.noreply.github.com>
2021-04-10 11:26:38 +0200

    Day 12 part 1; refactored to use simple arrays.

--- src/main/java/day12/NBodyProblem.java
+++ src/main/java/day12/NBodyProblem.java
@@ -5,45 +5,42 @@ import static java.lang.Math.abs;
 
 public class NBodyProblem {
 
-    public int part1(int[][] positions, int iterations) {
-        var moons = positions.length;
-        var axes = positions[0].length;
-
-        var vel = createVelocityVectors(moons, axes);
+    public int part1(int[][] pos, int iterations) {
+        var vel = createVelocityVectors(pos.length, pos[0].length);
         for (var i = 0; i < iterations; i++) {
             // apply gravity
-            for (var m = 0; m < moons; m++) {
-                for (var n = 0; n < moons; n++) {
-                    for (var a = 0; a < axes; a++) {
-                        vel[m][a] += signum(positions[n][a] - positions[m][a]);
+            for (var m = 0; m < pos.length; m++) {
+                for (var n = 0; n < pos.length; n++) {
+                    for (var a = 0; a < pos[m].length; a++) {
+                        vel[m][a] += signum(pos[n][a] - pos[m][a]);
                     }
                 }
             }
             // apply velocity
-            for (var m = 0; m < moons; m++) {
-                for (var a = 0; a < axes; a++) {
-                    positions[m][a] += vel[m][a];
+            for (var m = 0; m < pos.length; m++) {
+                for (var a = 0; a < pos[m].length; a++) {
+                    pos[m][a] += vel[m][a];
                 }
             }
         }
 
-        return calcTotalEnergy(positions, moons, axes, vel);
+        return calcTotalEnergy(pos, vel);
     }
 
-    private int[][] createVelocityVectors(int numMoons, int numAxes) {
-        int[][] vel = new int[numMoons][];
-        for (var m = 0; m < numMoons; m++) {
-            vel[m] = new int[numAxes];
+    private int[][] createVelocityVectors(int moons, int axes) {
+        int[][] vel = new int[moons][];
+        for (var m = 0; m < moons; m++) {
+            vel[m] = new int[axes];
         }
         return vel;
     }
 
-    private int calcTotalEnergy(int[][] positions, int numMoons, int numAxes, int[][] vel) {
+    private int calcTotalEnergy(int[][] positions, int[][] vel) {
         var total = 0;
-        for (var m = 0; m < numMoons; m++) {
+        for (var m = 0; m < positions.length; m++) {
             var potential = 0;
             var kinetic = 0;
-            for (var a = 0; a < numAxes; a++) {
+            for (var a = 0; a < positions[m].length; a++) {
                 potential += abs(positions[m][a]);
                 kinetic += abs(vel[m][a]);
             }
